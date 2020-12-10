8e2a4341896e2da58cc571696e7adb5cbe51e32f
HidiAlgo <hashan.mahesh01@gmail.com>
2020-12-11 00:59:32 +0530

    Dynamic Programming with java

--- src/Fibonachi.java
+++ src/Fibonachi.java
@@ -1,11 +1,11 @@
 import java.util.HashMap;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Fibonachi {
     private static Function<Integer, Long> fib1; // Time O(2^n) Space O(n)
-    private static Function<Integer, Long> fib2; // Time O(n) Space O(n)
+    private static BiFunction<Integer, HashMap<Integer, Long>, Long> fib2; // Time O(n) Space O(n)
 
-    private static HashMap<Integer, Long> memo = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -14,17 +14,15 @@ public class Fibonachi {
             return fib1.apply(n-1) + fib1.apply(n-2);
         };
 
-        fib2 = n -> {
+        fib2 = (n, memo) -> {
             if (memo.containsKey(n)) return memo.get(n);
             if (n<=2) return (long) 1;
-            Long result = fib2.apply(n-1) + fib2.apply(n-2);
+            Long result = fib2.apply(n-1, memo) + fib2.apply(n-2, memo);
             memo.put(n, result);
             return result;
         };
 
 
-        System.out.println(fib2.apply(6));
-        System.out.println(fib2.apply(50));
-        System.out.println(fib2.apply(80));
+        System.out.println(fib2.apply(50, new HashMap<>()));
     }
 }
